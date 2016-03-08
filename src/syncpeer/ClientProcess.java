4b48a3d9e7f35dd85645b2d94fffc3f1cbbf81ac
LuanWH <lwhluan@gmail.com>
2016-03-08 11:27:07 +0800

    Revert to previous version.

--- src/syncpeer/ClientProcess.java
+++ src/syncpeer/ClientProcess.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.NoRouteToHostException;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
@@ -162,10 +161,7 @@ class ClientProcess extends SyncProcess {
 			socket.close();
 			
 			System.out.println(name+": synchronization finished!");
-		}catch(NoRouteToHostException e){
-			System.out.print(name + ": Failed to establish connection to "+this.ipAddr+". ");
-			System.out.println(e.getMessage());			
-		}catch(SocketException|UnknownHostException e){
+		}catch(UnknownHostException|SocketException e){
 			System.out.print(name + ": Failed to establish connection to "+this.ipAddr+". ");
 			System.out.println(e.getMessage());
 		} catch (Exception e) {
